ea9311201a151f3aacf0f4f7a833e88e107eabf4
George <georgezhu211@gmail.com>
2021-05-02 18:58:45 -0500

    display messages in index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+// sample messages
+const messages = [
+  {
+    text: 'Hello World!',
+    user: 'stranger1',
+    added: new Date(),
+  },
+  {
+    text: 'Nice too meet you',
+    user: 'stranger2',
+    added: new Date(),
+  },
+  {
+    text: 'Goodbye World!',
+    user: 'stranger3',
+    added: new Date(),
+  },
+]
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { messages: messages })
+})
 
-module.exports = router;
+module.exports = router
